7ea58088a59b35d22fb69caff576138d0609c4e1
Karels ☔️ <karels@Karelss-MacBook-Pro.local>
2025-02-15 12:18:09 -0500

    update

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ new Swiper('.card-wrapper', {
         content = `
           <span class="service-title">Services aux enfants</span>
           <p>Nos services sont conçus pour soutenir le développement harmonieux de votre enfant en lui offrant des interventions personnalisées et les outils nécessaires à son bien-être. Notre objectif est d’accompagner chaque enfant de manière bienveillante, tout en respectant son rythme.</p>
-          <ul class="service-list">
+          <ul>
             <li>Soutien à l’intégration</li>
             <li>Stimulation du langage</li>
             <li>Stimulation précoce</li>
@@ -97,16 +97,18 @@ new Swiper('.card-wrapper', {
         `;
         break;
       default:
-        content = `<span class="service-title">Services aux milieux de garde & organismes </span>
-          <p>Nous avons pour objectif de vous offrir les ressources adaptées à vos besoins spécifiques, afin de maximiser l'efficacité de vos actions et leur impact.</p>
+        content = `
+          <span class="service-title">Services aux enfants</span>
+          <p>Nos services sont conçus pour soutenir le développement harmonieux de votre enfant en lui offrant des interventions personnalisées et les outils nécessaires à son bien-être. Notre objectif est d’accompagner chaque enfant de manière bienveillante, tout en respectant son rythme.</p>
           <ul>
-            <li>Soutien pédagogique pour renforcer les compétences de votre équipe</li>
-            <li>Atelier et formation pédagogique</li>
-            <li>Animation de groupe</li>
-            <li>Soutien dans vos initiatives éducatives</li>
+            <li>Soutien à l’intégration</li>
+            <li>Stimulation du langage</li>
+            <li>Stimulation précoce</li>
+            <li>Animation sur les habiletés sociales</li>
             <li>Outils pédagogiques adaptés</li>
           </ul>
-          <p>Nos rencontres se déroulent selon vos besoins et vos préférences : à votre domicile, en ligne, ou directement dans le milieu de garde ou scolaire de votre enfant. Nous intervenons dans la région de Montréal, incluant la Montérégie et la Rive-Nord.</p>`;
+          <p>Nos rencontres se déroulent selon vos besoins et vos préférences : à votre domicile, en ligne, ou directement dans le milieu de garde ou scolaire de votre enfant. Nous intervenons dans la région de Montréal, incluant la Montérégie et la Rive-Nord.</p>
+        `;
     }
     serviceContent.innerHTML = content;
   }
@@ -126,17 +128,14 @@ new Swiper('.card-wrapper', {
   document.addEventListener('DOMContentLoaded', function() {
   // Array of commitments
   const commitments = [
-    "Cette première rencontre, réalisée en visioconférence pour plus de flexibilité, nous permet de bien comprendre vos besoins,d’évaluer les services que nous pouvons offrir et de répondre à vos questions. Si nous convenons ensemble de poursuivre, nous procédons à l’ouverture du dossier.",
-    "L’éducateur spécialisé observe l’enfant dans son environnement habituel afin de recueillir des informations clés, repérer les problématiques et poser des hypothèses. Cette étape est essentielle pour cibler les besoins et préparer un accompagnement adapté.",
-    "En collaboration avec vous, et le cas échéant, les autres professionnels impliqués, nous partageons nos observations, priorisons les besoins et définissons ensemble les objectifs. Ces éléments sont formalisés dans un plan d’accompagnement personnalisé.",
-    "Nous mettons en œuvre les moyens définis et travaillons sur les objectifs fixés. Les suivis permettent également d’évaluer les progrès, d’ajuster les interventions et, si nécessaire, de réviser le plan. Ces rencontres peuvent se faire en présentiel ou en ligne, selon ce qui convient le mieux à votre situation.",
-  
-  ];
+    "Nous savons que l''équilibre entre vie professionnelle et personnelle est essentiel. C''est pourquoi nous offrons des horaires flexibles, adaptés à vos besoins et à votre secteur géographique, afin de vous permettre de travailler à votre rythme et dans un maximum de confort.",
+    "Nous offrons un salaire compétitif à partir de 28$/h, accompagné de primes et de révisions salariales régulières tout au long de votre carrière chez ASES. Nous reconnaissons et valorisons l'expertise que vous apportez et nous nous engageons à soutenir votre évolution professionnelle.",
+     "Rejoignez notre équipe pour une expérience de travail enrichissante, des opportunités de croissance à long terme et un environnement adapté à votre style de vie !"
+];
   const commitTitle = [
-    "Consultation d’orientation (en visioconférence)",
-    "Rencontre d’observation",
-    "Élaboration du plan d’accompagnement",
-    "Suivis réguliers",
+    "Flexibilité d'horaire et mandats sur mesure :",
+    "Salaire",
+    "Rejoignez notre équipe"
   ];
 
   // Get DOM elements
